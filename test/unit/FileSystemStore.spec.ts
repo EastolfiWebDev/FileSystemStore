e764adb60e61dafb18397c75c9ae13ae8d2821a9
eastolfi <astolfi_wotc@hotmail.com>
2017-10-04 09:22:22 +0000

    test: Add source TS testing

--- test/unit/FileSystemStore.spec.ts
+++ test/unit/FileSystemStore.spec.ts
@@ -1,79 +1,20 @@
-var expect = require("chai").expect,
-    fs = require("fs"),
-    rimraf = require("rimraf"),
-    MongoPortable = require("mongo-portable");
+import "mocha";
+import { expect } from "chai";
+import { MongoPortable } from "mongo-portable";
 
-var FileSystemStore;
-    
-if (!!process.env.test_coverage) {
-    FileSystemStore = require("../../test/coverage/lib/JSW-Logger.js").FileSystemStore;
-} else {
-    FileSystemStore = require("../../index.js").FileSystemStore;
-}
-
-const DDBB_NAME = "test_ddbb";
-const COLL_NAME = "test_collection";
-const DDBB_PATH = "data";
-
-const DOC = {
-    _id: "1111",
-    name: "John",
-    lastname: "Abruzzi"
-};
-
-var clearDataDir = function() {
-    rimraf.sync("db");
-    rimraf.sync("data");
-};
-
-var checkDir = function(dirName, exist, nFiles) {
-    var files = fs.readdirSync(dirName);
-    
-    if (exist) {
-        expect(files).to.exist;
-        
-        expect(files).to.be.instanceof(Array);
-        expect(files).to.have.length(nFiles);
-    } else {
-        expect(files).to.not.exist;
-    }
-};
-
-var checkFile = function(fileName, exist, empty) {
-    if (empty != null) {
-        empty = !!empty;
-        
-        fs.readFile(fileName, function(error, data) {
-            expect(error).to.not.exist;
-            expect(data).to.exist;
-            
-            if (empty) {
-                expect(data).to.have.length(0);
-            } else {
-                expect(data).to.have.length.above(0);
-            }
-        });
-    } else {
-        var file = fs.readFileSync(fileName);
-        
-        if (exist) {
-            expect(file).to.exist;
-        } else {
-            expect(file).to.not.exist;
-        }
-    }
-};
+import { TestHelper } from "./test.helper";
+import { FileSystemStore } from "../../index";
+// import { FileSystemStore } from "../../index";
 
 var db = null;
-var store = null;
 describe("FileSystemStore", function() {
     after(function() {
-        clearDataDir();
+        TestHelper.clearDataDir();
     });
     
     describe("#Constructor", function() {
         before(function() {
-            clearDataDir();
+            TestHelper.clearDataDir();
         });
         
         it("should have the dependencies ready", function() {
@@ -82,41 +23,35 @@ describe("FileSystemStore", function() {
         });
         
         it("should be able to instantiate a new store", function() {
-            var _store = new FileSystemStore();
-            
-            expect(_store).to.exist;
-            expect(_store.options).to.exist;
-            
-            expect(_store.options.ddbb_path).to.be.equal("db");
-            
-            checkDir("db", true, 0);
-            
-            store = new FileSystemStore({
-                ddbb_path: DDBB_PATH,
-                sync: true
-            });
+            // Store with default values
+            let store = new FileSystemStore();
             
             expect(store).to.exist;
             expect(store.options).to.exist;
             
-            expect(store.options.ddbb_path).to.be.equal(DDBB_PATH);
+            expect(store.options.ddbb_path).to.be.equal(TestHelper.DDBB_PATH_DEF);
             
-            checkDir("data", true, 0);
+            TestHelper.assertDir(TestHelper.DDBB_PATH_DEF, true, 0);
+            
+            // Store with custom values
+            store = TestHelper.createStore();
+            
+            expect(store).to.exist;
+            expect(store.options).to.exist;
+            
+            expect(store.options.ddbb_path).to.be.equal(TestHelper.DDBB_PATH);
+            
+            TestHelper.assertDir(TestHelper.DDBB_PATH, true, 0);
         });
     });
     
     describe("#Collections", function() {
         before(function() {
-            clearDataDir();
-            
-            db = new MongoPortable(DDBB_NAME);
-            
-            db.addStore(store);
+            db = TestHelper.init();
         });
         
         after(function() {
-            db.dropDatabase();
-            db = null;
+            db = TestHelper.clear(db);
         });
         
         it("should not have collections at first", function() {
@@ -130,15 +65,15 @@ describe("FileSystemStore", function() {
         
         describe("#Create", function() {
             it("should be able to create a collection", function() {
-                var coll = db.collection(COLL_NAME);
+                var coll = db.collection(TestHelper.COLL_NAME);
             
                 expect(coll).to.exist;
                 
-                expect(coll.name).to.be.equal(COLL_NAME);
+                expect(coll.name).to.be.equal(TestHelper.COLL_NAME);
                 expect(coll.docs).to.be.instanceof(Array);
                 expect(coll.docs).to.have.length(0);
                 
-                checkFile(`${DDBB_PATH}/${DDBB_NAME}/${COLL_NAME}.json`, true, true);
+                TestHelper.assertFile(TestHelper.buildCollectionPath(), true, true);
             });
         });
         
@@ -157,33 +92,30 @@ describe("FileSystemStore", function() {
     
     describe("#Documents", function() {
         before(function() {
-            db = new MongoPortable(DDBB_NAME);
-            
-            db.addStore(store);
+            db = TestHelper.init();
         });
         
         after(function() {
-            db.dropDatabase();
-            db = null;
+            db = TestHelper.clear(db);
         });
         
         describe("#Create", function() {
             it("should be able to create a document", function() {
-                var coll = db.collection(COLL_NAME);
+                var coll = db.collection(TestHelper.COLL_NAME);
                 
-                coll.insert(DOC);
+                coll.insert(TestHelper.createDocument());
                 
-                checkFile(`${DDBB_PATH}/${DDBB_NAME}/${COLL_NAME}.json`, true, false);
+                TestHelper.assertFile(TestHelper.buildCollectionPath(), true, false);
             });
         });
         
         describe("#Read", function() {
             before(function() {
-                fs.appendFileSync(`${DDBB_PATH}/${DDBB_NAME}/${COLL_NAME}.json`, "{\"_id\":\"0000\",\"name\":\"John\", \"lastname\": \"Wayne\"}");
+                TestHelper.appendDocument();
             });
             
             it("should be able to read a document", function() {
-                var coll = db.collection(COLL_NAME);
+                var coll = db.collection(TestHelper.COLL_NAME);
                 
                 var docs = coll.find({ name: "John" }).fetch();
                 
@@ -202,7 +134,7 @@ describe("FileSystemStore", function() {
         
         describe("#Update", function() {
             it("should be able to update a document", function() {
-                var coll = db.collection(COLL_NAME);
+                var coll = db.collection(TestHelper.COLL_NAME);
                 
                 coll.update({
                     lastname: "Wayne"
@@ -211,12 +143,10 @@ describe("FileSystemStore", function() {
                 });
                 
                 // Disconnecting the DDBB to check persistance
-                db.dropDatabase();
-                db = null;
-                db = new MongoPortable(DDBB_NAME);
-                db.addStore(store);
+                db = TestHelper.clear(db);
+                db = TestHelper.init(false);
                 
-                coll = db.collection(COLL_NAME);
+                coll = db.collection(TestHelper.COLL_NAME);
                 
                 var docs = coll.find({ name: "John" }).fetch();
                 
@@ -230,19 +160,17 @@ describe("FileSystemStore", function() {
         
         describe("#Delete", function() {
             it("should be able to delete a document", function() {
-                var coll = db.collection(COLL_NAME);
+                var coll = db.collection(TestHelper.COLL_NAME);
                 
                 coll.delete({
                     lastname: "Wayne"
                 });
                 
                 // Disconnecting the DDBB to check persistance
-                db.dropDatabase();
-                db = null;
-                db = new MongoPortable(DDBB_NAME);
-                db.addStore(store);
+                db = TestHelper.clear(db);
+                db = TestHelper.init(false);
                 
-                coll = db.collection(COLL_NAME);
+                coll = db.collection(TestHelper.COLL_NAME);
                 
                 var docs = coll.find().fetch();
                 
