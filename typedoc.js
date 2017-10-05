dd6af1dfb8d9e61efd1e0ccca8a9b35d400fa905
eastolfi <astolfi_wotc@hotmail.com>
2017-10-05 07:46:36 +0000

    doc: Change to typedoc

---
+++ typedoc.js
@@ -0,0 +1,8 @@
+//  --readme ./README.md --tsconfig ./tsconfig.json --mode file --out doc1 src/
+(function() {
+    return {
+        readme: "./README.md",
+        src: "./src",
+        out: "./doc1"
+    };
+})();
